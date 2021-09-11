458d27da7477720458defe42a5a2d0d122d353de
Fabricio2530 <fabricio.neri.ismart@gmail.com>
2021-09-11 18:36:09 -0300

    Alterações no Projeto 3

---
+++ Projeto_3/Client.py
@@ -0,0 +1,181 @@
+#####################################################
+# Camada Física da Computação
+#Carareto
+#11/08/2020
+#Aplicação
+####################################################
+
+
+#esta é a camada superior, de aplicação do seu software de comunicação serial UART.
+#para acompanhar a execução e identificar erros, construa prints ao longo do código! 
+
+import time
+import random 
+from enlace import *
+import numpy as np
+
+# voce deverá descomentar e configurar a porta com através da qual ira fazer comunicaçao
+#   para saber a sua porta, execute no terminal :
+#   python -m serial.tools.list_ports
+# se estiver usando windows, o gerenciador de dispositivos informa a porta
+
+#use uma das 3 opcoes para atribuir à variável a porta usada
+#serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
+#serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
+serialName1 = "COM4"    
+             # Windows(variacao de)
+
+def DATAGRAMA(n_id, mensagemInteira):
+    #criando o bit inicial de toda mensagem
+    package = bytearray(b'\x01')
+    #adicionando o id
+    package.append(n_id)
+    
+    if n_id == 0:
+        #adicionando o tamanho do PayLoad
+        package.append(0)
+        #adicionando o tamanho do EOP
+        package.append(4)
+        #adicionando se é ou não handshake
+        package.append(1)
+        #adicionando o total de pacotes
+        package.append(len(mensagemInteira))
+        #adicionando 4 bytes vazios para completar o HEAD
+        for num in range(4):
+            package.append(0)
+
+        #vamos adicionar agora o PayLoad
+        package.append(0)
+
+        #vamos adicionar o restante: o EOP
+        for num in range(2):
+            package.append(2)
+            package.append(4)
+    
+    else:
+        #adicionando o tamanho do PayLoad
+        n = n_id-1
+        print('O tamanho do payload é de {}\n'.format(len(mensagemInteira[n])))
+        
+        package.append(len(mensagemInteira[n]))
+        #adicionando o tamanho do EOP
+        package.append(4)
+        #adicionando se é ou não handshake
+        package.append(0)
+        #adicionando o total de pacotes
+        package.append(len(mensagemInteira))
+        #adicionando 4 bytes vazios para completar o HEAD
+        for num in range(4):
+            package.append(0)
+
+        #vamos adicionar agora o PayLoad
+        package += (mensagemInteira[n_id-1])
+
+        #vamos adicionar o restante: o EOP
+        for num in range(2):
+            package.append(2)
+            package.append(4)
+
+    return package
+        
+
+    
+
+def main():
+    try:
+        #declaramos um objeto do tipo enlace com o nome "com". Essa é a camada inferior à aplicação. Observe que um parametro
+        #para declarar esse objeto é o nome da porta.
+
+
+        print('iniciando a comunicação')
+        print('-----------------------')
+        
+        #iniciando o CLIENT
+        com1 = enlace(serialName1)
+        com1.enable()
+       
+        #lendo o arquivo que será enviado para o SERVER    
+        with open('lua.jpg', 'rb') as binaryImage:
+            binImage = binaryImage.read()
+        
+        #criando variáveis importantes
+        fragmentacao = False
+        NUM_ID = 0
+        TAMANHO_PAYLOAD = 114
+        
+        #lista de PAYLOADS
+        FRAGMENTO = [binImage[i: i+TAMANHO_PAYLOAD] for i in range(0, len(binImage), TAMANHO_PAYLOAD)]
+
+        print("O total de pacotes necessários será de {}".format(len(FRAGMENTO)))
+        print('------------------------')
+
+        mensagem = DATAGRAMA(NUM_ID, FRAGMENTO) 
+
+        print("Mensagem feita com sucesso!!")
+        print("Vamos enviar o seguinte handshake:\n {}".format(mensagem))
+        com1.sendData(mensagem)
+        
+        handshake = True
+
+        while handshake:
+            response, nResponse = com1.getData(15)
+            print(response)
+            if ((response == "")):
+                question = input("Você quer tentar de novo? S/N\n")
+                if question == "S":
+                    pass
+                else:
+                    break
+            else:
+
+                print("---------------------------")
+                print("HandShake feito com sucesso")
+                print(len(response))
+                handshake = False
+                fragmentacao = True 
+                NUM_ID += 1
+        '''
+            LOOP para envio do conteúdo da mensagem
+        '''
+        while fragmentacao:
+            print("---------------------------")
+            print("Estamos no package de id: {}".format(NUM_ID))
+            if (NUM_ID >= 3):
+                fragmentacao = False
+            else:
+                mensagem = DATAGRAMA(NUM_ID, FRAGMENTO)
+                print("---------------------------")
+                print("O seguinte pacote foi criado:")
+                print(mensagem)
+                print("---------------------------")
+                print("O pacote tem tamanho {}".format(len(mensagem)))
+                
+                #AGORA É PRECISO ENVIAR A MENSAGEM
+                com1.sendData(mensagem)
+                waitResponse = True 
+                while waitResponse:
+                    response, nresponse = com1.getData(15)
+                    nPacote = response[1]
+                    resultadoEnvio = response(4)
+
+                    if resultadoEnvio == 1:
+                        NUM_ID += 1
+                        waitResponse = False
+                    
+                    else:
+                        waitResponse = False
+
+        print('terminando a comunicação')
+        print('------------------------')
+        com1.disable()
+       
+        
+    except Exception as erro:
+        print("ops! :-\\")
+        print(erro)
+        com1.disable()
+        
+
+    #so roda o main quando for executado do terminal ... se for chamado dentro de outro modulo nao roda
+if __name__ == "__main__":
+    main()
---
+++ Projeto_3/Server.py
@@ -0,0 +1,165 @@
+#####################################################
+# Camada Física da Computação
+#Carareto
+#11/08/2020
+#Aplicação
+####################################################
+
+def DATAGRAMA(nPacote, resposta=0):
+    package = bytearray(b'\x01')
+    package.append(nPacote) #numero do pacote
+    package.append(0) #tamanho do payload
+    package.append(4) 
+    package.append(resposta)#resposta do servidor se deu certo ou nao
+    package.append(0) #Como servidor não precisa de payload, o numero de pacotes totais é 0
+    for num in range(4):
+        package.append(0)
+    #adicionando o payload
+    package.append(0)
+    #adicionando o EOP
+    for num in range(2):
+        package.append(2)
+        package.append(4)
+        
+    return package
+
+#esta é a camada superior, de aplicação do seu software de comunicação serial UART.
+#para acompanhar a execução e identificar erros, construa prints ao longo do código! 
+
+import time
+from enlace import *
+import time
+import numpy as np
+
+import random
+
+# voce deverá descomentar e configurar a porta com através da qual ira fazer comunicaçao
+#   para saber a sua porta, execute no terminal :
+#   python -m serial.tools.list_ports
+# se estiver usando windows, o gerenciador de dispositivos informa a porta
+
+#use uma das 3 opcoes para atribuir à variável a porta usada
+#serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
+#serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
+serialName1 = "COM4"
+serialName2 = "COM5"  
+                # Windows(variacao de)
+def computeBytes(bytearray):
+    sum = 0
+    for i in bytearray:
+        sum += len(bytearray)
+    return sum
+
+def main():
+    try:
+        com2 = enlace(serialName2)
+        com2.enable()
+        print("Comunicação iniciada com sucesso")
+        print("---------------------------")
+        
+     
+        EOP_PADRAO = bytearray()
+        EOP_PADRAO.append(2)
+        EOP_PADRAO.append(4)
+        EOP_PADRAO.append(2)
+        EOP_PADRAO.append(4)
+
+
+        '''
+            ESPERANDO PELO HANDSHAKE
+        '''
+        #lendo a primeira parte da mensagem, o HEAD
+        
+        rxBuffer, nRx = com2.getData(10)
+        
+        if rxBuffer[4] == 1:
+            print("HandShake recebido!!\n")
+            #COM BASE NO HEAD DO HANDSHAKE, VAMOS CRIAR AS VARIAVEIS DA MENSAGEM
+            NUM_ID = rxBuffer[1]
+            TAMANHO_PAYLOAD = rxBuffer[2]
+            TAMANHO_EOP = rxBuffer[3]
+            TOTAL_PACKAGES = rxBuffer[5]
+            print("SERAO RECEBIDOS AO TODO {}".format(TOTAL_PACKAGES))
+            MENSAGEM = bytearray(b'\x01')
+
+        TOTAL_PAYLOAD = bytearray()
+        #Vamos terminar a leitura da primeira mensagem
+
+        #mesmo se for dito que não tem nada no PAYLOAD, vai haver um valor 0
+
+        if TAMANHO_PAYLOAD != 0:
+            rxBuffer, nRx = com2.getData(TAMANHO_PAYLOAD)
+        else:
+            rxBuffer, nRx = com2.getData(1)
+        print("PAYLOAD RECEBIDO NO HANDSHAKE:\n {}".format(rxBuffer))
+
+        rxBuffer, nRx = com2.getData(TAMANHO_EOP)
+        print("EOP RECEBIDO NO HANDSHAKE:\n {}".format(rxBuffer))
+
+        #AGORA QUE JÁ VERIFICAMOS TUDO, PRECISAMOS RESPONDER O CLIENT
+        response  = DATAGRAMA(NUM_ID, 1)
+        com2.sendData(response)
+        time.sleep(0.5)
+
+        '''
+            recepção da mensagem
+        '''
+
+        while NUM_ID <= 2:
+            print("Estamos no package {}\n".format(NUM_ID))
+            #vamos tentar receber o primeiro pacote
+            time.sleep(1)
+            mensagem, nMensagem = com2.getData(10) #primeiro temos que ler o HEAD, ele quem diz quantos BYTES temos
+            print(mensagem)
+            print(mensagem[0])
+            if (mensagem[0] == b'\x01') or (mensagem[0] == 1):
+                if (mensagem[1] <= NUM_ID):
+                    print('FOI ENVIADO O MESMO PACOTE, POR FAVOR ENVIAR O PRÓXIMO\n')
+
+                else:
+                    print('byte inicial recebido com sucesso\n')
+                    tamanhoPayLoad = mensagem[2]
+                    print('O tamanho do payload é de {}\n'.format(tamanhoPayLoad))
+                    payload, npayLoad = com2.getData(tamanhoPayLoad)
+                    print('Esse foi o payload recebido: \n{}\n'.format(payload))
+                    tamanhoEOP = mensagem[3]
+                    print('O tamanho do EOP é de {}\n'.format(tamanhoEOP))
+                    eop, nEOP = com2.getData(tamanhoEOP)
+                    print('Esse foi o payload recebido: \n{}\n'.format(eop))
+
+                    if eop == EOP_PADRAO:
+                        print('O EOP foi recebido CORRETAMENTE\n')
+                        TOTAL_PAYLOAD += payload
+                        NUM_ID += 1
+                        mensagem = DATAGRAMA(NUM_ID, 1)
+                        #agora é preciso enviar a mensagem dizendo que deu tudo certo!!
+
+
+                    else:
+                        print('O EOP NÃO FOI ENVIADO DA MANEIRA ADEQUADA, POR FAVOR REENVIAR O PACOTE\n')
+
+            else:
+                print('Houve um erro com o envio do pacote')        
+
+        
+
+        
+
+
+
+        
+        print("-------------------------")
+        print("Comunicação encerrada")
+        print("-------------------------")
+
+        com2.disable()
+        
+    except Exception as erro:
+        print("ops! :-\\")
+        print(erro)
+        com2.disable()
+        
+
+    #so roda o main quando for executado do terminal ... se for chamado dentro de outro modulo nao roda
+if __name__ == "__main__":
+    main()
